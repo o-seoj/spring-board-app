5d7fc302659ff57903faa67a726da15bba31a5ea
o-seoj <tjwjd010@naver.com>
2025-09-26 11:15:19 +0900

    add sboard

---
+++ src/main/java/kr/co/sboard/controller/FindController.java
@@ -0,0 +1,24 @@
+package kr.co.sboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FindController {
+    @GetMapping("/find/changePassword")
+    public String changePassworld(){
+        return "find/changePassword";
+    }
+    @GetMapping("/find/password")
+    public String passworld(){
+        return "find/password";
+    }
+    @GetMapping("/find/resultUserId")
+    public String resultUserId(){
+        return "find/resultUserId";
+    }
+    @GetMapping("/find/userId")
+    public String userId(){
+        return "find/userId";
+    }
+}
