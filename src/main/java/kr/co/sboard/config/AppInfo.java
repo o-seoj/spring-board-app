f7971e75222b86e9ef6d8cc54293c7c452918d7b
o-seoj <tjwjd010@naver.com>
2025-09-26 12:49:26 +0900

    version 0.0.1;

--- src/main/java/kr/co/sboard/config/AppInfo.java
+++ src/main/java/kr/co/sboard/config/AppInfo.java
@@ -1,6 +1,9 @@
 package kr.co.sboard.config;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 
 @Data
