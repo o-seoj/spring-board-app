9faf815684955b840c4e1b5d95cf862edbfb2a1c
o-seoj <tjwjd010@naver.com>
2025-09-30 14:05:15 +0900

    add version 0.0.4

--- src/test/java/kr/co/sboard/service/ArticleServiceTest.java
+++ src/test/java/kr/co/sboard/service/ArticleServiceTest.java
@@ -2,12 +2,15 @@ package kr.co.sboard.service;
 
 import kr.co.sboard.dto.ArticleDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ArticleServiceTest {
+
+    @Autowired
     private ArticleService articleService;
 
     @Test
