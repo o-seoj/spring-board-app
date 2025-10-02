75f0526bca1b920829a9d678374866cb615e2c76
o-seoj <tjwjd010@naver.com>
2025-10-02 10:51:49 +0900

    version 0.0.1

--- src/main/java/kr/co/sboard/controller/ArticleController.java
+++ src/main/java/kr/co/sboard/controller/ArticleController.java
@@ -29,8 +29,14 @@ public class ArticleController {
 
     @GetMapping("/article/list")
     public String list(Model model, PageRequestDTO pageRequestDTO) {
-       PageResponseDTO pageResponseDTO = articleService.getArticleAll(pageRequestDTO);
-       model.addAttribute(pageResponseDTO);
+
+        //JPA
+        // PageResponseDTO pageResponseDTO = articleService.getArticleAll(pageRequestDTO);
+
+        // Mybatis
+        PageResponseDTO pageResponseDTO = articleService.selectArticleAll(pageRequestDTO);
+
+        model.addAttribute(pageResponseDTO);
        return "article/list";
     }
 
